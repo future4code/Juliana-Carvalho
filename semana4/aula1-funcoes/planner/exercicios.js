e6505393b6c65c0b613b6e73a2f608073a73805b
julianadpnobregac <julianadpnobregac@gmail.com>
2019-11-19 15:19:55 -0300

    Primeiros passos cadastro novos alunos

---
+++ semana4/aula1-funcoes/planner/exercicios.js
@@ -0,0 +1,6 @@
+function sorriaefalexiss(fotoa,fotob,fotoc) {
+    const array [fotoa,fotob,fotoc]
+        console.log (fotoa,fotob,fotoc)
+       }
+    
+       sorriaefalexiss (fotoc, fotob, fotoa)
